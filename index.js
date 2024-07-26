aafec72c98d81e640906a44c1f4b1c4dedcec322
aamidshah <shahaamid@33.com>
2024-07-26 08:51:54 +0530

    node2

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-// creating first web server  using
-// model view controller to manage code
+
 require('dotenv').config()
 const express = require("express");
-const { type } = require("os");
-// a third party middleware
-const morgan = require("morgan");
 const mongoose = require('mongoose')
 const cors = require('cors')
-const {Schema} = mongoose;
 const path = require('path')
 
-// create a server
 const server = express();
 
-// import  diff api routes
 const productRouter = require("./routes/product");
 const userRouter = require("./routes/user");
 console.log(process.env.DB_PASSWORD )
 
 
-// db connection 
+ 
 
 main().catch(err => console.log(err));
 
@@ -31,9 +24,7 @@ async function main() {
 
 
 
-// body parser
 server.use(express.json());
-// server.use(express.static(process.env.PUBLIC_DIR));
 server.use(express.static(path.join(__dirname, process.env.PUBLIC_DIR,'build')));
 server.use(cors());
 server.use("/products", productRouter.router);
@@ -42,7 +33,6 @@ server.use('*',(req,res)=>{
   console.log(path.join(__dirname, 'public', 'build', 'index.html'));
   res.sendFile(path.join(__dirname, process.env.PUBLIC_DIR, 'build', 'index.html'));
 
-  // res.sendFile('/public/build/index.html')
 }
 )
 
