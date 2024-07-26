26180a3e44c3043fccc9cfbab25dadaa48303e58
aamidshah <shahaamid@33.com>
2024-07-26 14:31:34 +0530

    node

--- controller/product.js
+++ controller/product.js
@@ -29,6 +29,9 @@ exports.getProduct = async (req, res) => {
   const id = req.params.id;
   try {
     const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
     res.json(product);
   } catch (err) {
     res.status(500).json({ error: err.message });
