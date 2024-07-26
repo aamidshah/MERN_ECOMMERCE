6f2097458c2228800512a13bb35c122088ecc6a9
aamidshah <shahaamid@33.com>
2024-07-26 09:58:30 +0530

    node2

---
+++ public/index.js
@@ -0,0 +1,43 @@
+
+require('dotenv').config()
+const express = require("express");
+const mongoose = require('mongoose')
+const cors = require('cors')
+const path = require('path')
+
+const server = express();
+
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+console.log(process.env.DB_PASSWORD )
+
+
+ 
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("connected to db")
+}
+
+
+
+
+server.use(express.json());
+server.use(express.static(path.join(__dirname, process.env.PUBLIC_DIR,'build')));
+server.use(cors());
+server.use("/products", productRouter.router);
+server.use("/users", userRouter.router);
+server.use('*',(req,res)=>{
+  console.log(path.join(__dirname, 'public', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, process.env.PUBLIC_DIR, 'build', 'index.html'));
+
+}
+)
+
+
+
+server.listen(process.env.PORT, () => {
+  console.log("server started");
+});
